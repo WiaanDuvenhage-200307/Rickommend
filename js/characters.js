db137fad017334953da0f339adaded859db10826
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-01 20:49:13 +0200

    Footer + Char Page +  pagnation
    Added new logo to footer. Updated the charaters page that you can see all the characters and added pagnation to the page.

--- js/characters.js
+++ js/characters.js
@@ -2,16 +2,20 @@ const API = "https://rickandmortyapi.com/api/character";
 
 $.getJSON(API, (result) =>{
     console.log(result)
-    for(i = 0; i < 50;i++){
+    for(i = 0; i < result.results.length;i++){
         
 
         $("#card-info").append(` 
-        <div class="card-img" style="width:20%; background-color: "red"; height: 250px">
-        <img class="char-img" src="${result.results[i].image}">
-        <p>${result.results[i].name}</p>
-        </div>
+
+            <div class="card-img" style="width:20%; background-color: "red"; height: 250px">
+                <img class="char-img" src="${result.results[i].image}">
+                <p class="char-name">${result.results[i].name}</p>
+            </div>
+
         `)
       
     };
+
+    
 
 })
