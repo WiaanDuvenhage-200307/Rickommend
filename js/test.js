1142a8534ae192f3a95c363773451fc5a5ed7843
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-08 14:19:24 +0200

    css file create + test func +
    Created separate .css files for each page and added light functionality to see what button I am pressing and displaying to the page if it is right or wrong.

--- js/test.js
+++ js/test.js
@@ -1,12 +1,16 @@
-$(function(){
+$(document).ready(function(){
 
+    $('button').click(function(){
+        var clickedBtn  = $(this).val();
+        console.log(clickedBtn); // to check what button is being clicked and display it to the console.
 
-    $(button).click(()=>{
-        var clickedBtn = $(this).val();
-        console.log(clickedBtn);
-    });
-    
-
+        if(clickedBtn == 70){
+            $(this).css('background-color: green');
+            $('.output').append("Correct");
+        } else {
+            $(this).css('background-color: red');
+            $('.output').append('Wrong!');
+        }
+    })
 })
- 
  
