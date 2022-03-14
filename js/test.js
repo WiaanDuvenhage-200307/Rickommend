de3cbaa578eab548835599dc70484a059de708d3
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-14 12:31:42 +0200

    Changed Questions + Changed json img
    Updated styling on test page. Added @media queries and changed json questions and images.

--- js/test.js
+++ js/test.js
@@ -12,19 +12,21 @@ $(document).ready(function(){
             $('.opacity').addClass([i]);
             $(".q-block").append(`
 
-            <div class="opacity ${json.Questions[i].id}">
+            <div class="${json.Questions[i].id}">
             <h1 style="color: black;" class="q">${json.Questions[i].Question}</h1>
 
             <button id="69" class="button a" value="69"><p>${json.Questions[i].Option1}</p></button>
             <button id="103" class="button a" value="103"><p>${json.Questions[i].Option2}</p></button>
             <button id="45" class="button a" value="45"><p>${json.Questions[i].Option3}</p></button>
             <button id="70" class="button a" value="70"><p>${json.Questions[i].Option4}</p></button>
-
-
-            </div>
-            <div class="output"></div>
+            <br/>
+            <img src = "${json.Questions[i].image}"/>
 
             `)
+
+           
+
+            
 
             // !Hiding and showing questions onClick
             $('.first').fadeIn();
@@ -59,8 +61,9 @@ $(document).ready(function(){
                 $('.first').hide();
                 $('.second').hide();
                 $('.third').hide();
+                $('.q-block').hide();
 
-                $('.output').text("You are a lot like Rick!");
+                $('.output').text("Thank you for completing the test!");
 
 
                 $.getJSON("../videos.json", function(json){
@@ -73,7 +76,7 @@ $(document).ready(function(){
                     var episodeName = episodeID.episodeName;
 
                     $('.recc').append(`
-                    <button class="button"x><h1><a href="${episodeURL}">You should watch '${episodeName}'</a></h1></button>
+                    <button class="button"x><h2><a href="${episodeURL}"> WATCH: '${episodeName}'</a></h2></button>
                     `)
         
                     
