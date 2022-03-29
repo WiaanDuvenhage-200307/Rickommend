849c0bfd1ce2d95e3b1927ba061095c8b3ff1f4d
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-29 08:54:25 +0200

    removed comments

--- contactform.php
+++ contactform.php
@@ -1,12 +1,12 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "feedback@rickommend.co.za"; // this must be the email address from your hosting provider
+    $to = "feedback@rickommend.co.za"; 
 
     $subject = "SEO Project Feedback";
     $message = $_POST['feedback'];
 
     mail($to,$subject,$message);
 
-    header('Location: http://www.rickommend.co.za"); // you can add a thank you page if you like, this redirects to home
+    header('Location: http://www.rickommend.co.za");
     }
 ?>
